c9c2cbcc3e4b95e836359107a323c3463e1850db
jimmimmim <likecharry@gmail.com>
2021-08-02 03:43:34 +0900

    update navigation bar

--- script.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function() {
-//     $(".dropdown").mouseover(function() { 
-//         $(this).children(".dropdown-content").stop().slideDown(); 
-//     }); 
-//     $(".dropdown").mouseleave(function() {
-//          $(this).children(".dropdown-content").stop().slideUp(); 
-//     }); 
-// });
-
