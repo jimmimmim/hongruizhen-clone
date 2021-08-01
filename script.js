2915f7cc9a0c56ef7cec9ac773e727931b672b59
jimmimmim <likecharry@gmail.com>
2021-08-01 23:59:23 +0900

    add dropdown in top nav

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     $(".dropdown").mouseover(function() { 
+//         $(this).children(".dropdown-content").stop().slideDown(); 
+//     }); 
+//     $(".dropdown").mouseleave(function() {
+//          $(this).children(".dropdown-content").stop().slideUp(); 
+//     }); 
+// });
+
